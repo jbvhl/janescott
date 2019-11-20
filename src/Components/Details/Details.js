74399ce7db8dc13dc13892c70916b27509715ca9
B L <jbvhl@hotmail.com>
2019-11-19 21:49:30 -0700

    server added

--- src/Components/Details/Details.js
+++ src/Components/Details/Details.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./details.css";
 
 export default class Details extends React.Component {
   render() {
     return (
-      <div>
+      <div className="details">
         <h1>Details</h1>
       </div>
     );
